c3217d30d341299d5c25e083de7dfebe30cd4c66
ednacoveja <ovejerocande@gmail.com>
2022-12-06 18:25:37 -0300

    auth0 profile home and landing

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from 'react-router-dom';
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Auth0Provider
+    domain="dev-dac6aiwfsuui8sza.us.auth0.com"
+    clientId="Zq57VTmktJlPeucm0IiWzBRNvI9uqhhc"
+    redirectUri={window.location.origin}
+  >
+    <Provider store={store}>
+      <React.StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </React.StrictMode>
+    </Provider>
+  </Auth0Provider>,
   document.getElementById("root")
 );
 
