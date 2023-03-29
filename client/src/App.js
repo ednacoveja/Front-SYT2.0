a3028ce92a2c269a348be26b4db724cf4878aca8
ednacoveja <ovejerocande@gmail.com>
2023-03-29 11:51:03 -0300

    renderizacion de componentes

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,29 @@
-import './App.css';
-import { Route } from 'react-router-dom';
-import Landing from './view/Landing/Landing';
-import About from './view/About/About';
-import Admin from './view/Admin/Admin';
-import Claim from './view/Claim/Claim';
-import Home from './view/Home/Home';
-import Premium from './view/Premium/Premium';
-import Profile from './view/Profile/Profile';
-import ReportUser from './view/ReportUser/ReportUser';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Landing from "./view/Landing/Landing";
+import About from "./view/About/About";
+import Admin from "./view/Admin/Admin";
+import Claim from "./view/Claim/Claim";
+import Home from "./view/Home/Home";
+import Premium from "./view/Premium/Premium";
+import Profile from "./view/Profile/Profile";
+import ReportUser from "./view/ReportUser/ReportUser";
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/"  component={Landing} />
-      <Route exact path="/About"  component={About} />
-      <Route exact path="/Admin"  component={Admin} />
-      <Route exact path="/Claim"  component={Claim} />
-      <Route exact path="/Home"  component={Home} />
-      <Route exact path="/Premium"  component={Premium} />
-      <Route exact path="/Profile"  component={Profile} />
-      <Route exact path="/ReportUser"  component={ReportUser} />
+      <Routes>
+        <Route exact path="/" element={<Landing/>} />
+        <Route exact path="/About" element={<About/>} />
+        <Route exact path="/Admin" element={<Admin/>} />
+        <Route exact path="/Claim" element={<Claim/>} />
+        <Route exact path="/Home" element={<Home/>} />
+        <Route exact path="/Premium" element={<Premium/>} />
+        <Route exact path="/Profile" element={<Profile/>} />
+        <Route exact path="/ReportUser" element={<ReportUser/>} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
-
